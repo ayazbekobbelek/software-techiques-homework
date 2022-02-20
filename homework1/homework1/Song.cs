85be7cd481281b80f16a11dd7680f0469436ec35
Belek Ayazbekov <belek.ayazbekov@nokia.com>
2022-02-20 21:05:09 +0100

    First Exercise

---
+++ homework1/homework1/Song.cs
@@ -0,0 +1,19 @@
+﻿class Song
+{
+    public readonly string Artist;
+    public readonly string Title;
+
+    public Song(string artist, string title)
+    {
+        Artist = artist;
+        Title = title;
+    }
+    public override string ToString()
+    {
+        return string.Format("{0}: {1}", Artist, Title);
+    }
+}
+
+
+
+
